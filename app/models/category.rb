e2dec1563bb8d19c4942e5dcb72a25a12cfd7312
Dmitry Ovcharenko <ovcharenkodmitry@gmail.com>
2012-07-03 12:01:34 +0400

    first

---
+++ app/models/category.rb
@@ -0,0 +1,7 @@
+class Category < ActiveRecord::Base
+  include ActsAsTree
+  acts_as_tree order: "name"
+
+
+attr_accessible :name, :parent_id
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Menu::Application.config.secret_token = '6a4bdb6001ea297c01160d5af690f1287c9dd2f5aa23591ffd68ae2d3a946a0799d06f075bc0f5bf77d7a96993d01ca8ecb9af99b424123bb03c24e6676c39ac'
